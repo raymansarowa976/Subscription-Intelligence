7d478d67739c2b38aca7f98fdc7230e5e1f747cb
raymansarowa976 <126209800+raymansarowa976@users.noreply.github.com>
2026-04-06 00:21:25 -0700

    updated urls

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
+def root_redirect(request):
+    return redirect('accounts:signup')
+
 urlpatterns = [
+    path('', root_redirect, name='home'),
     path('admin/', admin.site.urls),  # Keep this one
     path('accounts/', include(('users.auth.urls', 'accounts'), namespace='accounts')),
 ]
