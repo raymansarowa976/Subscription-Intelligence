f11c6eaa61c985d7be53f8cf688dec9af0e7f196
raymansarowa976 <126209800+raymansarowa976@users.noreply.github.com>
2026-04-09 18:21:24 -0700

    to deal with inactive accounts

--- users/auth/authentication_forms.py
+++ users/auth/authentication_forms.py
@@ -1,4 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+
+
+User = get_user_model()
 
 
 class SubscriptionAuthenticationForm(AuthenticationForm):
@@ -9,3 +14,17 @@ class SubscriptionAuthenticationForm(AuthenticationForm):
         ),
         "inactive": "This account is inactive.",
     }
+
+    def clean(self):
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        if username and password:
+            user = User.objects.filter(username=username).first()
+            if user and user.check_password(password) and not user.is_active:
+                raise ValidationError(
+                    self.error_messages["inactive"],
+                    code="inactive",
+                )
+
+        return super().clean()
