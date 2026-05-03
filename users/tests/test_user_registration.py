540aa2f50d3098f0d2bd106bf3213c66986cdb0e
raymansarowa976 <126209800+raymansarowa976@users.noreply.github.com>
2026-05-02 21:40:08 -0700

    updated registration and security changes to account for our new username and password reset features

--- users/tests/test_user_registration.py
+++ users/tests/test_user_registration.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test.utils import override_settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
 from django.urls import reverse
 from django.core import mail
 import re
@@ -13,6 +14,8 @@ class RegistrationTest(TestCase):
         self.verify_url = reverse('accounts:verify_token')
         self.resend_url = reverse('accounts:resend_token')
         self.login_url = reverse('accounts:login')
+        self.forgot_username_url = reverse('accounts:forgot_username')
+        self.forgot_password_url = reverse('accounts:forgot_password')
         self.user_data = {
             'first_name': 'Taylor',
             'last_name': 'Jordan',
@@ -85,3 +88,76 @@ class RegistrationTest(TestCase):
 
         self.assertRedirects(response, self.verify_url)
         self.assertEqual(len(mail.outbox), 1)
+
+    def test_forgot_username_emails_associated_username(self):
+        User.objects.create_user(
+            username='recoveruser',
+            email='recover@gmail.com',
+            password='Complex123!',
+            is_active=True,
+        )
+
+        response = self.client.post(
+            self.forgot_username_url,
+            {'email': 'RECOVER@gmail.com'},
+            follow=True,
+        )
+
+        self.assertRedirects(response, self.login_url)
+        self.assertContains(response, 'Your username has been sent')
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertIn('username', mail.outbox[0].subject.lower())
+        self.assertIn('recoveruser', mail.outbox[0].body)
+        self.assertNotIn('Complex123!', mail.outbox[0].body)
+
+    def test_forgot_username_tells_user_when_email_has_no_account(self):
+        response = self.client.post(
+            self.forgot_username_url,
+            {'email': 'missing@gmail.com'},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No account is associated with this email address.')
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_forgot_password_sends_temporary_password_and_replaces_old_password(self):
+        user = User.objects.create_user(
+            username='resetuser',
+            email='reset@gmail.com',
+            password='Complex123!',
+            is_active=True,
+        )
+
+        response = self.client.post(
+            self.forgot_password_url,
+            {'email': 'reset@gmail.com'},
+            follow=True,
+        )
+
+        self.assertRedirects(response, self.login_url)
+        self.assertContains(response, 'A temporary password has been sent')
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertIn('temporary', mail.outbox[0].subject.lower())
+        self.assertNotIn('Complex123!', mail.outbox[0].body)
+
+        temporary_password = re.search(r'Your temporary password is: ([^\n]+)', mail.outbox[0].body).group(1)
+        validate_password(temporary_password, user)
+        user.refresh_from_db()
+        self.assertFalse(user.check_password('Complex123!'))
+        self.assertTrue(user.check_password(temporary_password))
+
+        login_response = self.client.post(
+            self.login_url,
+            {'username': user.username, 'password': temporary_password},
+        )
+        self.assertRedirects(login_response, self.verify_url)
+
+    def test_forgot_password_tells_user_when_email_has_no_account(self):
+        response = self.client.post(
+            self.forgot_password_url,
+            {'email': 'missing@gmail.com'},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No account is associated with this email address.')
+        self.assertEqual(len(mail.outbox), 0)
