be4e1314b5f91370147e45ede728f815b54bd062
raymansarowa976 <126209800+raymansarowa976@users.noreply.github.com>
2026-05-20 00:01:08 -0700

    added contact page, might need to make smal changes to it later

--- config/urls.py
+++ config/urls.py
@@ -5,8 +5,12 @@ from django.urls import path, include
 def landing_page(request):
     return render(request, "landing.html")
 
+def contact_page(request):
+    return render(request, "contact.html")
+
 urlpatterns = [
     path('', landing_page, name='home'),
+    path('contact/', contact_page, name='contact'),
     path('admin/', admin.site.urls),  # Keep this one
     path('dashboard/', include('subscriptions.urls')),
     path('transactions/', include(('subscriptions.transaction_urls', 'transactions'), namespace='transactions')),
