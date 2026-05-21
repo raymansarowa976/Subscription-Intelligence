20818b9a9f92e91d2ddcbdda6f8c9d31d7aecf9f
raymansarowa976 <126209800+raymansarowa976@users.noreply.github.com>
2026-05-20 18:39:27 -0700

    added updated backend

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 
-from .views import dashboard_view, scan_inbox_view, subscription_results_view
+from .views import (
+    analytics_view,
+    dashboard_view,
+    data_sources_view,
+    gmail_integrations_view,
+    scan_inbox_view,
+    subscription_results_view,
+)
 
 
 urlpatterns = [
     path("", dashboard_view, name="dashboard"),
+    path("gmail/", gmail_integrations_view, name="gmail_integrations"),
+    path("analytics/", analytics_view, name="analytics"),
+    path("data-sources/", data_sources_view, name="data_sources"),
     path("subscriptions/", subscription_results_view, name="subscription_results"),
     path("email/scan/", scan_inbox_view, name="scan_inbox"),
 ]
